74048074b4b5de0f65a926a2ed5a15c6e449180f
Kevin Contreras Rodríguez <kvncont@gmail.com>
2025-03-16 06:41:41 +0000

    feat: add output for Lambda role name in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "lambda_role_arn" {
   description = "ARN Role for the Lambda function"
 }
 
+output "lambda_role_name" {
+  value       = aws_iam_role.app.name
+  description = "Role name for the Lambda function"
+}
+
 output "function_url" {
   value       = resource.aws_lambda_function_url.app.function_url
   description = "Lambda function URL"
